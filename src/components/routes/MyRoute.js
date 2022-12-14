24ad37f674c2446c5e4a71c8a5781b063b9beef0
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-12-14 16:00:12 +0600

    Pages, routes and navbar component create

---
+++ src/components/routes/MyRoute.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyRoute extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default MyRoute;
